3d22ad95c5d36ae3431fc177ca1169e56c37ea05
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-04-14 22:33:45 +0100

    Now returns all the payee labels tied to payments in data.gmdsp.org

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,7 @@
 export default
 Ember.Route.extend({
     model: function () {
-
-        /*
-         First get all the payees stored in the database against any payment type
-
-         This is the equivalent sparql:
-
-
-         */
-
+        
         var siteDomain = "data.gmdsp.org.uk"
         var query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
             + "SELECT DISTINCT ?payee ?payeeLabel WHERE {"
@@ -20,11 +12,8 @@ Ember.Route.extend({
         var url = "http://" + siteDomain + "/sparql.json?query=";
         url += encodeURIComponent(query);
 
-        return Ember.$.getJSON(
-            url,
-            {},
-            function(data){
-                "use strict";
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            Ember.$.getJSON(url).then(function(data){
                 var results = data["results"]["bindings"]
                 var numOfResults = results.length;
                 var nameArray = [];
@@ -32,9 +21,8 @@ Ember.Route.extend({
                 for (var i=0; i<numOfResults;i++){
                     nameArray.push(results[i]["payeeLabel"]["value"]);
                 }
-                console.log(nameArray);
-                return nameArray;
-            }
-        );
+                resolve(nameArray);
+            });
+        });
     }
 });
