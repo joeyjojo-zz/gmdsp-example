d329d00a10a18d186d63548c471d187ecd92f34b
Jon Dunleavy <joeyjojosmith@gmail.com>
2014-05-02 19:52:41 +0100

    Now outputs total amount on transaction page

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,8 +1,13 @@
 var TransactionsController = Ember.ArrayController.extend({
     getTransactionTotal: function(){
         "use strict";
-        return "200.00";
-    }.property("getTransactionTotal")
+        var transactions = this.get("content");
+        return transactions.reduce(function(previousValue, transaction){
+            return numeral(
+                numeral().unformat(previousValue) + numeral().unformat(transaction.amount)
+            ).format('£0.00');
+        }, '£0.00');
+    }.property()
 });
 
 export default TransactionsController;
